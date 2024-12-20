8213bafac6b9067774874ab6b381aa1e175ab0ef
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-20 06:33:52 +0530

    Update _layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { Slot } from "expo-router";
+import { Slot, useSegments } from "expo-router";
 
 // Import your global CSS file
 import "../global.css";
+import { useAuth } from '../context/authContext';
+
+const MainLayout=()=>{
+    const{isAuthenticated}=useAuth();
+    const segments=useSegments();
+}
 
 export default Slot;
