ec4eaaf0c9a8c84c12e190fba0d515962e3c5158
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-20 06:41:44 +0530

    Update _layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -1,14 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Slot, useSegments } from "expo-router";
+import { View, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { Slot, useSegments } from 'expo-router';
+import '../global.css';
+import { AuthContextProvider, useAuth } from '../context/authContext';
 
-// Import your global CSS file
-import "../global.css";
-import { useAuth } from '../context/authContext';
+const MainLayout = () => {
+  const { isAuthenticated } = useAuth();
+  const segments = useSegments();
 
-const MainLayout=()=>{
-    const{isAuthenticated}=useAuth();
-    const segments=useSegments();
+  useEffect(() => {
+    // Check if user is authenticated or not
+    [isAuthenticated];
+  }, [isAuthenticated]);
+
+  return <Slot />;
+};
+
+export default function RootLayout() {
+  return (
+    <AuthContextProvider>
+      <MainLayout />
+    </AuthContextProvider>
+  );
 }
-
-export default Slot;
