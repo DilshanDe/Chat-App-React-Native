0018f1cfa0f09f4fb851d49a168474a0a3a5caa0
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-20 16:11:49 +0530

    Update signin.js

--- app/signin.js
+++ app/signin.js
@@ -2,6 +2,7 @@ import { View, Text, Image } from 'react-native'
 import React from 'react'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import { StatusBar } from 'expo-status-bar';
+import { Octicons } from '@expo/vector-icons';
 
 export default function SignIn() {
   return (
@@ -16,7 +17,12 @@ export default function SignIn() {
 
 
         <View className='gap-10'>
-          <Text style={{fontSize:hp(4)}} className='font-bold'>Sign In</Text>
+          <Text style={{fontSize:hp(4)}} className='font-bold tracking-wider text-center text-neutral-800'>Sign In</Text>
+
+          {/*inputs*/}
+          <View style={{height:hp(7)}} className='flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl'>
+            <Octicons name='mail'size={hp(2.7)} color='gray'/>
+          </View>
         </View>
       </View>
       
