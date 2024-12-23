fd95d8e607ff8c16027c7b14f835cfb86c80d291
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-23 13:52:25 +0530

    Update home.js

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useAuth } from '../../context/authContext'
+import { Button } from 'react-native-web';
 
 export default function Home() {
+  const{logout}=useAuth();
   return (
     <View>
       <Text>Home</Text>
+      <Button title=' Sign Out'/>
     </View>
   )
 }
