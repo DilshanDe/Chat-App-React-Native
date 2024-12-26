54ef97e502d48c15d08ab09a293835adaf7c2947
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-26 17:59:59 +0530

    Update CustomMenuItems.js

--- components/CustomMenuItems.js
+++ components/CustomMenuItems.js
@@ -8,9 +8,10 @@ export const MenuItem=({text,action,value,icon})=>{
     return(
         <MenuOption onSelect={()=>action(value)}>
             <View className="px-4 py-1 flex-row justify-between items-center">
-                <Text>icon</Text>
-                <Text>text</Text>
+                <Text style={{fontSize:hp(1.8)}} className='font-semibold text-neutral-600'>{text}</Text>
+                {icon}
             </View>
+            
 
         </MenuOption>
 
