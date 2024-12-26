cf93a0ce99716e4e7c192a0037e338654a121885
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-26 17:59:46 +0530

    Update home.js

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,24 +1,40 @@
-import { View, Text, Pressable, StatusBar } from 'react-native'
+import { View, Text, Pressable, StatusBar, ActivityIndicator } from 'react-native'
 import React from 'react'
 import { useAuth } from '../../context/authContext'
-import { Button } from 'react-native-web';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+import Loading from '../../components/Loading';
+import ChatList from '../../components/ChatList';
 
 export default function Home() {
   const{logout,user}=useAuth();
-  const handleLogout= async ()=>{
-    await logout();
+  const[sers,setUsers]=([1,2,3]);
+  const getUsers=async()=>{
+    //fetach users
 
   }
-  console.log('user data: ',user);
+  
 
   return (
     <View className='flex-1 bg-white'>
       <StatusBar barStyle="dark-content" backgroundColor="white" />
+
+      {
+        users.length>0?(
+          <ChatList users={users}/>
+
+        ):(
+          <View className='flex items-center' style={{top:hp(30)}}>
+              <ActivityIndicator size='large'/>
+
+           {/*<Loading size={hp(10)}/>*/}
+
+            </View>
+        )
+      }
       
-      <Text>Home</Text>
-      <Pressable onPress={handleLogout}>
-        <Text>Sign Out</Text>
-      </Pressable>
+      
+      
     </View>
   )
 }
