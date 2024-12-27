376c247c2366386995d0a1c1df7a913dfa9f2890
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-27 08:33:38 +0530

    Update ChatItem.js

--- components/ChatItem.js
+++ components/ChatItem.js
@@ -4,9 +4,15 @@ import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-nativ
 
 export default function ChatItem({item}) {
   return (
-   <TouchableOpacity>
+   <TouchableOpacity className='flex-row justify-between mx-4 items-center gap-3 mb-4 pb-2 border-b border-b-neutral-200'>
     <Image
-      source={require('../assets/images/avatar.png')}style={{height:hp(6),aspectRatio:1}}/>
+      source={require('../assets/images/avatar.png')}
+      style={{height:hp(6),aspectRatio:1}}
+      className='rounded-full'
+      />
+
+
+      {/*name and last massege*/}
    </TouchableOpacity>
   )
 }
