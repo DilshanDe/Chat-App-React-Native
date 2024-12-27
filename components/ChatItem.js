ba5f0af749d59df514b1e5f5f08d0b5883955815
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-27 08:27:48 +0530

    Update ChatItem.js

--- components/ChatItem.js
+++ components/ChatItem.js
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
-export default function ChatItem() {
+export default function ChatItem({item}) {
   return (
-    <View>
-      <Text>ChatItem</Text>
-    </View>
+   <TouchableOpacity>
+    <Image
+      source={require('../assets/images/avatar.png')}style={{height:hp(6),aspectRatio:1}}/>
+   </TouchableOpacity>
   )
 }
