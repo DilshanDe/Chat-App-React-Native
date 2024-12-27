1a1f909d16645e0d66f3771eb11d075f786c884d
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-27 07:06:25 +0530

    Update ChatList.js

--- components/ChatList.js
+++ components/ChatList.js
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
+import ChatItem from './ChatItem'
 
-export default function ChatList({Users}) {
+export default function ChatList({users}) {
   return (
-    <View>
-      <Text>ChatList</Text>
+    <View className='flex-1'>
+      <FlatList
+        data={users}
+        contentContainerStyle={{flex:1,paddingVertical:25}}
+        keyExtractor={item=>Math.random()}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item,index})=><ChatItem item={item}/>}
+
+        />
     </View>
   )
 }
