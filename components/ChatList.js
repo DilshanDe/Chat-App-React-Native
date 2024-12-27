3f3522c9a49341eb81cf960a971c074e140b9944
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-27 08:27:52 +0530

    Update ChatList.js

--- components/ChatList.js
+++ components/ChatList.js
@@ -10,7 +10,7 @@ export default function ChatList({users}) {
         contentContainerStyle={{flex:1,paddingVertical:25}}
         keyExtractor={item=>Math.random()}
         showsVerticalScrollIndicator={false}
-        renderItem={({item,index})=><ChatItem item={item}/>}
+        renderItem={({item,index})=><ChatItem item={item} index={index}/>}
 
         />
     </View>
