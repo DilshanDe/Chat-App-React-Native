cdb62453fde933befc6ae1a190915c77b373c163
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-28 09:28:50 +0530

    Update chatRoom.js

--- app/(app)/chatRoom.js
+++ app/(app)/chatRoom.js
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useLocalSearchParams } from 'expo-router'
 
 export default function ChatRoom() {
+  const item=useLocalSearchParams();
+  console.log('got item data: ',item);
   return (
     <View>
       <Text>ChatRoom</Text>
