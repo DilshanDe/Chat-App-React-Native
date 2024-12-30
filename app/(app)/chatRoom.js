a44c9318d70b5a4d84289498a9af5d9423979cbf
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-30 16:16:06 +0530

    Update chatRoom.js

--- app/(app)/chatRoom.js
+++ app/(app)/chatRoom.js
@@ -1,16 +1,62 @@
-import { View, Text, StatusBar, TextInput, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, StatusBar, TextInput, TouchableOpacity, Alert } from 'react-native'
+import React, { useRef, useState } from 'react'
 import { useLocalSearchParams, useRouter } from 'expo-router'
 import ChatRoomHeader from '../../components/ChatRoomHeader';
 import MessageList from '../../components/MessageList';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import { Feather } from '@expo/vector-icons';
 import CustomKeyboardView from '../../components/CustomKeyboardView';
+import { getRoomId } from '../../utils/comman';
+import { addDoc, collection, doc, setDoc, Timestamp } from 'firebase/firestore';
+import { db } from '../../firebaseConfig';
+import { useEffect } from 'react';
+import { useAuth } from '../../context/authContext';
 
 export default function ChatRoom() {
   const item=useLocalSearchParams();
+  const { user } = useAuth(); // Logged-in user
   const router=useRouter();
   const[messages,setMessages]=useState([]);
+  const textRef=useRef('');
+  const inputRef=useRef(null);
+  useEffect(() => {
+    createRoomIfNoExits();
+  }, []);
+
+  const createRoomIfNoExits = async () => {
+    // Generate room ID
+    let roomId = getRoomId(user?.userId, item?.userId);
+    await setDoc(doc(db, "rooms", roomId), {
+      roomId,
+      createdAt: Timestamp.fromDate(new Date()),
+    });
+  };
+
+  const handleSendMessage= async()=>{
+    let message=textRef.current.trim();
+    if(!message) return;
+    try{
+      let roomId=getRoomId(user?.userId,item?.userId);
+      const docRef=doc(db,'rooms',roomId);
+      const messagesRef=collection(docRef,"messages");
+      textRef.current='';
+      if(inputRef) inputRef?.current?.clear();
+
+      const newDoc=await addDoc(messagesRef,{
+        userId:user?.userId,
+        text:message,
+        profileUrl:user?.profileUrl,
+        senderName:user?.username,
+        createdAt:Timestamp.fromDate(new Date())
+      });
+
+      console.log('new message id',newDoc.id);
+
+    }catch(err){
+      Alert.alert('Message',err.message);
+    }
+
+  }
   return (
     <CustomKeyboardView inChat={true}>
     <View className='flex-1 bg-white'>
@@ -28,12 +74,14 @@ export default function ChatRoom() {
           
             <View className='flex-row mx-3 justify-between bg-white  boeder p-2 border-neutral-300 rounded-full pl-5'>
               <TextInput
+                ref={inputRef}
+                onChangeText={value=>textRef.current=value}
                 placeholder='Type message...'
                 style={{fontSize:hp(2)}}
                 className='flex-1 mr-2'
                 />
 
-                <TouchableOpacity className='bg-neutral-200 p-2 mr-[1px] rounded-full'>
+                <TouchableOpacity onPress={handleSendMessage} className='bg-neutral-200 p-2 mr-[1px] rounded-full'>
                   <Feather name='send'size={hp(2.7)}color='#737373'/>
                 </TouchableOpacity>
             </View>
