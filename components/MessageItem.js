79b20e30affd6c81ac0ab28ba5aa3061c1e9e2e6
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-31 07:27:34 +0530

    Update MessageItem.js

--- components/MessageItem.js
+++ components/MessageItem.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function MessageItem() {
-  return (
-    <View>
-      <Text>MessageItem</Text>
-    </View>
-  )
+export default function MessageItem({message,currentUser}) {
+ if(currentUser?.userId==message?.userId){
+    //my message
+    return(
+        <View></View>
+    )
+ }
 }
