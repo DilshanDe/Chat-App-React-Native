2ba911c17e8f43b14521b70e3410f5470d36a725
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2025-01-02 08:04:43 +0530

    Update home.js

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -39,7 +39,7 @@ export default function Home() {
 
       {
         users.length>0?(
-          <ChatList users={users}/>
+          <ChatList currentUser={user} users={users}/>
 
         ):(
           <View className='flex items-center' style={{top:hp(30)}}>
